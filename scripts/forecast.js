b685ed15fc19813b8d997ad4ab1303981d11fb2e
Zbigniew Kierpiec <zbigniewkierpiec@gmail.com>
2020-08-01 10:26:39 +0100

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+const key='KckmVdQziFJcJ4eY2KpyG499HsywPuwN'
+// weatcher informatons
+const getWeather= async (id) =>{
+
+const base='http://dataservice.accuweather.com/currentconditions/v1/'
+const query =`${id}?apikey=${key}`
+
+const response = await fetch(base+query);
+const data=await response.json();
+
+return data[0];
+}
+const getCity= async (city)=>{
+
+const base='http://dataservice.accuweather.com/locations/v1/cities/search'
+const query=`?apikey=${key}&q=${city}`
+
+
+const response = await fetch(base+query);
+const data= await response.json();
+return data[0];
+};
+
+
